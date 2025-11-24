320953e2e35b2bba48d2fae882046afced23ba44
olovj222 <ol.juengt@duocuc.cl>
2025-11-24 17:42:56 -0300

    1st commit

---
+++ src/test/java/com/pixelium/levelup/LevelupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelium.levelup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
