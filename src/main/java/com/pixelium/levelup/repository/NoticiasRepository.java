f492054e1b523f74ab2d315cb9e5b499feca5b6c
TremorSongg <jua.riverag@duocuc.cl>
2025-11-24 18:06:03 -0300

    Controller, service agregados

--- src/main/java/com/pixelium/levelup/repository/NoticiasRepository.java
+++ src/main/java/com/pixelium/levelup/repository/NoticiasRepository.java
@@ -1,4 +1,4 @@
 package com.pixelium.levelup.repository;
 
-public interface ProductoRepository {
+public interface NoticiasRepository {
 }
---
+++ src/main/java/com/pixelium/levelup/service/NoticiasService.java
@@ -0,0 +1,4 @@
+package com.pixelium.levelup.service;
+
+public class NoticiasService {
+}
